f0ec788b661943cb7b2da3cc031bfafa3c8783b0
Daniel Pacak <pacak.daniel@gmail.com>
2018-05-16 13:56:41 +0200

    chore: cleanup

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = {
   REGON: require('./lib/regon'),
   PESEL: require('./lib/pesel'),
   NIP: require('./lib/nip'),
-  IdentityCard: require('./lib/identity_card.js')
+  IdentityCard: require('./lib/identity_card')
 };
